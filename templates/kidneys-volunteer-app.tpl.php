dda9bf7bc75ec7ec949bbff51156fa5687663809
Jacob Lane <lane.jacob@gmail.com>
2019-04-17 20:41:20 -0400

    adding geo field debugging to template

--- templates/kidneys-volunteer-app.tpl.php
+++ templates/kidneys-volunteer-app.tpl.php
@@ -1,4 +1,17 @@
-<div class="container padding-x--md">
-  testing
-  <?php print $form;?>
+<div class="container">
+  <div class="display--flex flex-wrap--wrap">
+    <div class="sm--width--50 width--100 padding-bottom--lg sm--padding-right--lg">
+      <div class="margin-bottom--xl">
+        <h2 class="pane-title">
+          Sign Up to Volunteer
+        </h2>
+        <p>Thank you for your interest in becoming a volunteer. We just need some information from you to get started. After you complete the form we'll send you an email so you know we've received your information. Next, someone from our volunteer office will contact you.</p>
+      </div>
+      <div class="bg--white rounded  border border-color--gray-5 border-width--sm padding--md">
+        <?php print $form;?>
+      </div>
+    </div>
+    <div class="sm--width--50 width--100 padding-bottom--lg sm--padding-left--lg">
+    </div>
+  </div>
 </div>
